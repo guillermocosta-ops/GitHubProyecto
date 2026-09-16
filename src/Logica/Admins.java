9ece875a13aecf8fd30cb7c0dd9348920947e371
guillermocosta-ops <guillermo.costa@docente.ceibal.edu.uy>
2026-09-16 10:32:50 -0300

    XDxdxdqdasdas

--- src/Logica/Admins.java
+++ src/Logica/Admins.java
@@ -1,13 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Logica;
 
-/**
- *
- * @author guill
- */
+package Logica;
+import java.util.ArrayList;
+import java.util.List;
 public class Admins {
-    
+       private final List<Admin> listaAdmins;
+
+    public Admins() {
+        listaAdmins = new ArrayList<>();
+    }
+
+    public List<Admin> getListaAdmins() {
+        return List.copyOf(listaAdmins);
+    }
+
+    public void agregarAdmin(Admin admin) {
+        listaAdmins.add(admin);
+    }
+
+    public void eliminarAdmin(Admin admin) {
+        listaAdmins.remove(admin);
+    }
 }
